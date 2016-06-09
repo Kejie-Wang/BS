f9fbd36557b84d5875dc8c550ba3b132046e60f4
JackWang822 <wang_kejie@foxmail.com>
2016-06-09 20:22:25 +0800

    add the home page and sign in page

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+//view enginer setup @jack
+var ejs = require('ejs');
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/login', function(req, res, next){
+  res.render('login');
+});
+
+router.post('/auth/login', function(req, res, next){
+	console.log(req.body);
 });
 
 module.exports = router;
